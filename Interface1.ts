82a32db18b5015b995e1d8e7ca0674bdf88967c0
Tilak <tilakreddy18@gmail.com>
2026-05-25 10:11:22 +0530

    InterfaceFile

--- Interface1.ts
+++ Interface1.ts
@@ -1,7 +1,20 @@
-export function getSessionDuration(session) {
+interface UserFeedback {
+    rating: number;
+    comment: string;
+}
+
+interface SupportSession {
+    id: string;
+    startedAt: number;
+    endedAt: number;
+    feedback: UserFeedback;
+}
+
+export function getSessionDuration(session: SupportSession): number {
     return session.endedAt - session.startedAt;
 }
-const session = {
+
+const session: SupportSession = {
     id: "101",
     startedAt: 100,
     endedAt: 1000,
@@ -10,4 +23,5 @@ const session = {
         comment: "Excellent Support"
     }
 };
+
 console.log(getSessionDuration(session));
